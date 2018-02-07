6e4a2fb6d1097044e8b40790598890ca9bc1776f
patricio <patricio87209@gmail.com>
2018-02-07 16:16:41 -0300

    Se valida correctamente la cantidad de estados

--- src/main/java/chasqui/view/composer/Constantes.java
+++ src/main/java/chasqui/view/composer/Constantes.java
@@ -7,6 +7,9 @@ public class Constantes {
 	//public final static String EMAIL_REGEX_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$;";
 	public final static Integer VENTANA_MODO_EDICION = 1;
 	public final static Integer VENTANA_MODO_LECTURA = 2;
+	
+	//Cantidad de estados de pedido posibles.
+	public final static Integer CANTIDAD_ESTADOS= 7;
 	public final static String ESTADO_PEDIDO_INEXISTENTE="INEXISTENTE";
 	public final static String ESTADO_PEDIDO_ABIERTO="ABIERTO";
 	public final static String ESTADO_PEDIDO_VENCIDO="VENCIDO";
