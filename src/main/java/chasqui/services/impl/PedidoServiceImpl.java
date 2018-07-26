3abd29347f64cba5057e1d8f4d95cf195ffa973f
DavidEGaleano <degnexo@gmail.com>
2018-07-26 17:41:07 -0300

    fix al eliminar productos

--- src/main/java/chasqui/services/impl/PedidoServiceImpl.java
+++ src/main/java/chasqui/services/impl/PedidoServiceImpl.java
@@ -273,22 +273,30 @@ public class PedidoServiceImpl implements PedidoService {
 			throws ProductoInexistenteException, RequestIncorrectoException, PedidoVigenteException, UsuarioInexistenteException {
 
 		validarRequest(request);
-
-		Cliente cliente = (Cliente) usuarioService.obtenerUsuarioPorEmail(email);
-		usuarioService.inicializarPedidos(cliente);
+		Pedido pedido = pedidoDAO.obtenerPedidoPorId(request.getIdPedido());
 		
 		Variante variante = productoService.obtenerVariantePor(request.getIdVariante());
-		validarParaEliminar(variante, cliente, request);
+		validarParaEliminar(variante, pedido, request);
 		
-		cliente.eliminarProductoEnPedido(request.getIdVariante(), variante.getPrecio(), request.getIdPedido(),
+		this.eliminarProductoEnPedido(request.getIdVariante(), variante.getPrecio(), pedido,
 				request.getCantidad());
 		
 		variante.eliminarReserva(request.getCantidad());
 		
-		usuarioService.guardarUsuario(cliente);
+		pedidoDAO.guardar(pedido);
 		
 		
 		productoService.modificarVariante(variante);
+	}
+	
+	public void eliminarProductoEnPedido(Integer idVariante, Double precio, Pedido p, Integer cantidad) {
+		ProductoPedido pp = p.encontrarProductoPedido(idVariante);
+		if (cantidad < pp.getCantidad()) {
+			pp.restar(cantidad);
+		} else {
+			p.eliminar(pp);
+		}
+		p.restarAlMontoActual(precio, cantidad);
 	}
 	
 
@@ -384,9 +392,9 @@ public class PedidoServiceImpl implements PedidoService {
 
 	}
 
-	private void validarParaEliminar(Variante v, Cliente c, AgregarQuitarProductoAPedidoRequest request)
+	private void validarParaEliminar(Variante v, Pedido p, AgregarQuitarProductoAPedidoRequest request)
 			throws ProductoInexistenteException, RequestIncorrectoException, PedidoVigenteException {
-		validacionesGenerales(v, c, request);
+		validacionesGenerales(v, request);
 		
 		Pedido pedido = this.obtenerPedidosporId(request.getIdPedido());
 		
@@ -394,14 +402,30 @@ public class PedidoServiceImpl implements PedidoService {
 			throw new PedidoVigenteException("El pedido se encuentra vencido y no es posible modificarlo");
 		}
 		
-		if (!c.contieneProductoEnPedido(v, request.getIdPedido())) {
+		if (!this.contieneProductoEnPedido(v, p)) {
 			throw new ProductoInexistenteException(
 					"El usuario no tiene el producto con ID " + request.getIdVariante() + " en el pedido");
 		}
-		if (!c.contieneCantidadDeProductoEnPedido(v, request.getIdPedido(), request.getCantidad())) {
+		if (!this.contieneCantidadDeProductoEnPedido(v, p, request.getCantidad())) {
 			throw new ProductoInexistenteException(
 					"No se puede quitar mas cantidad de un producto de la que el usuario posee en su pedido");
 		}
+	}
+	
+	private boolean contieneProductoEnPedido(Variante v, Pedido p) {
+		ProductoPedido pp = p.encontrarProductoPedido(v.getId());
+		if (pp != null && pp.getIdVariante().equals(v.getId())) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean contieneCantidadDeProductoEnPedido(Variante v, Pedido p, Integer cantidad) {
+		ProductoPedido pp = p.encontrarProductoPedido(v.getId());
+		if (pp.getIdVariante().equals(v.getId()) && cantidad <= pp.getCantidad()) {
+			return true;
+		}
+		return false;
 	}
 
 	private void validar(Variante v, Cliente c, AgregarQuitarProductoAPedidoRequest request)
@@ -459,7 +483,7 @@ public class PedidoServiceImpl implements PedidoService {
 //		}
 	}
 	
-	private void validacionesGenerales(Variante v, Cliente c, AgregarQuitarProductoAPedidoRequest request)
+	private void validacionesGenerales(Variante v, AgregarQuitarProductoAPedidoRequest request)
 			throws ProductoInexistenteException, PedidoVigenteException, RequestIncorrectoException {
 		if (v == null) {
 			throw new ProductoInexistenteException("No existe el producto con ID: " + request.getIdVariante());
@@ -468,16 +492,16 @@ public class PedidoServiceImpl implements PedidoService {
 		// mejorar
 		// (Hackaso)
 		//!c.contienePedido(request.getIdPedido()) &&
-		if (p == null || !c.contienePedidoAbiertoOCanceladoParaVendedor(p.getIdVendedor())) {
+		if (p == null) {
 			throw new PedidoVigenteException("El usuario no posee el pedido con ID:" + request.getIdPedido()
 			+ " o el mismo no se encuentra vigente ni cancelado");
 		}
 
-		if (!c.tienePedidoDeVendedor(p.getIdVendedor(), request.getIdPedido())) {
-			throw new RequestIncorrectoException(
-					"El producto no corresponde con el vendedor al que se le hizo el pedido con ID: "
-							+ request.getIdPedido());
-		}
+//		if (!c.tienePedidoDeVendedor(p.getIdVendedor(), request.getIdPedido())) {
+//			throw new RequestIncorrectoException(
+//					"El producto no corresponde con el vendedor al que se le hizo el pedido con ID: "
+//							+ request.getIdPedido());
+//		}
 	}
 	
 
