f9d8f48585917d13fa76143b8180249f017efe34
Leonardo Pavanello <leopavan@gmail.com>
2021-05-27 18:14:03 -0300

    elimina imports no necesarios

--- src/main/java/chasqui/dtos/queries/ProductoPedidoQueryDTO.java
+++ src/main/java/chasqui/dtos/queries/ProductoPedidoQueryDTO.java
@@ -1,16 +1,5 @@
 package chasqui.dtos.queries;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import chasqui.model.Direccion;
-import chasqui.model.GrupoCC;
-import chasqui.model.Pedido;
-import chasqui.model.PedidoColectivo;
-import chasqui.model.ProductoPedido;
-import chasqui.model.Zona;
-import chasqui.view.composer.Constantes;
-
 public class ProductoPedidoQueryDTO {
   private Integer idVariante;
   private String estado;
